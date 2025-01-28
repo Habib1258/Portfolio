5a118d7b8cd2166594e97043769c4fa11017f4a0
Habib1258 <habib054mustapha@gmail.com>
2025-01-28 10:57:34 +0100

    changed vite config

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import ghPages from 'vite-plugin-gh-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),  ghPages()],
   base: '/Portfolio/', // Ensure this matches your repo name
   server: {
     port: 3000,
